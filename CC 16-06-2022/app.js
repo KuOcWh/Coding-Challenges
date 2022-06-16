598d9dfcefc20eba6b22bc9fd2334ebcc3f5a7d0
Kurtis Ockenden-White <kurtisockenden-white@Kurtiss-MacBook-Air.local>
2022-06-16 01:10:19 +0100

    question answered

--- CC 16-06-2022/app.js
+++ CC 16-06-2022/app.js
@@ -15,3 +15,20 @@
 //5  =>  4
 //15  =>  13
 //-3  =>  -3
+
+
+function getRealFloor(n) {
+    if(n < 0){
+      return n
+    }else if(n === 0){
+      return 0
+    }else if(n === 1){
+      return 0
+    }else if(n < 13){
+      return n - 1
+    }else if(n === 15){
+      return 13
+    }else{
+      return n - 2
+    }
+  }
